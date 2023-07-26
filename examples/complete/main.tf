700d7a16aa0b6a25565d6cee802906499163143d
Felipe Tomazelli <fefetl08@gmail.com>
2023-07-26 20:00:46 -0300

    docs: add outputs values to example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -2,3 +2,23 @@ module "network_parameters" {
   source = "../.."
   path   = "/network/"
 }
+
+output "vpc_id" {
+  value = module.network_parameters.values["vpc_id"]
+}
+
+output "vpc_cidr_block" {
+  value = module.network_parameters.values["vpc_cidr_block"]
+}
+
+output "public_subnet_ids" {
+  value = module.network_parameters.values["public_subnet_ids"]
+}
+
+output "private_subnet_ids" {
+  value = module.network_parameters.values["private_subnet_ids"]
+}
+
+output "network_private_token" {
+  value = module.network_parameters.sensitive_values["network_private_token"]
+}
